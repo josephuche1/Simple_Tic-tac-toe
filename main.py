9cc3768b9f4017531a31b819225740b666627e7a
PARSIVALJ <uchejesusboy@gmail.com>
2023-02-26 13:08:50 +0200

    created a program to output the current state of the game

---
+++ main.py
@@ -0,0 +1,13 @@
+# program for simple tic tac toe
+
+game_state = input()
+start = 0
+stop = 3
+print("---------")
+while stop <= len(game_state):
+    row = game_state[start:stop]
+    formatted_row = " ".join(row)
+    print(f"| {formatted_row} |")
+    start = stop
+    stop = start + 3
+print("---------")
